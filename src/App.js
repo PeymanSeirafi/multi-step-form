b013e94077cd07f4ea187193627131d727217f7c
Peyman Seirafi <91782017+PeymanSeirafi@users.noreply.github.com>
2023-09-06 16:12:28 +0330

    Validating first step

--- src/App.js
+++ src/App.js
@@ -10,10 +10,23 @@ function App() {
     { key: 4, id: 4, name: "SUMMARY", active: false },
   ]);
 
+  function nextStep(step) {
+    setSteps((prev) => {
+      let ans = [];
+      for (let i of prev) {
+        if (i.id !== step) {
+          ans.push({ ...i, active: false });
+        } else {
+          ans.push({ ...i, active: true });
+        }
+      }
+      return ans;
+    });
+  }
   return (
     <div className="bg-white rounded-xl drop-shadow-xl p-5 pr-20 flex gap-20">
       <Sidebar steps={steps} />
-      <PersonalInfo />
+      <PersonalInfo func={nextStep} />
     </div>
   );
 }
--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,6 +1,62 @@
 import React from "react";
 
-function PersonalInfo() {
+function PersonalInfo({ func }) {
+  function validate() {
+    const nameBox = document.getElementById("name");
+    const emailBox = document.getElementById("email");
+    const numberBox = document.getElementById("number");
+
+    const name = nameBox.value;
+    const email = emailBox.value;
+    const number = numberBox.value;
+
+    const nameError = document.getElementById("nameError");
+    const emailError = document.getElementById("emailError");
+    const numberError = document.getElementById("numberError");
+
+    let isValied = true;
+
+    if (!/\S/.test(name)) {
+      nameBox.classList.add("border-strawberry");
+      nameError.innerHTML = "This field is required";
+      isValied = false;
+    } else {
+      nameBox.classList.remove("border-strawberry");
+      nameError.innerHTML = "";
+    }
+
+    if (!/\S/.test(email)) {
+      emailBox.classList.add("border-strawberry");
+      emailError.innerHTML = "This field is required";
+      isValied = false;
+    } else if (
+      !/^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_-]+)(\.[a-zA-Z]{2,5}){1,2}$/.test(email)
+    ) {
+      emailBox.classList.add("border-strawberry");
+      emailError.innerHTML = "Please enter valid email";
+      isValied = false;
+    } else {
+      emailBox.classList.remove("border-strawberry");
+      emailError.innerHTML = "";
+    }
+
+    if (!/\S/.test(number)) {
+      numberBox.classList.add("border-strawberry");
+      numberError.innerHTML = "This field is required";
+      isValied = false;
+    } else if (!/^\+[0-9]{8,16}$/.test(number.split(" ").join(""))) {
+      numberBox.classList.add("border-strawberry");
+      numberError.innerHTML = "Please enter valid number";
+      isValied = false;
+    } else {
+      numberBox.classList.remove("border-strawberry");
+      numberError.innerHTML = "";
+    }
+
+    if (isValied) {
+      func(2);
+    }
+  }
   return (
     <div className="flex flex-col justify-between pt-10">
       <div>
@@ -10,10 +66,14 @@ function PersonalInfo() {
         </h4>
         <div className="mt-10 text-[#355379] flex flex-col gap-6 font-[500]">
           <div className="flex flex-col gap-1">
-            <div>
+            <div className="flex justify-between">
               <label htmlFor="name" className="text-sm">
                 Name
               </label>
+              <div
+                className="text-sm text-strawberry font-[700]"
+                id="nameError"
+              ></div>
             </div>
             <input
               placeholder="e.g. Stephen King"
@@ -22,10 +82,14 @@ function PersonalInfo() {
             />
           </div>
           <div className="flex flex-col gap-1">
-            <div>
+            <div className="flex justify-between">
               <label htmlFor="email" className="text-sm">
                 Email Address
               </label>
+              <div
+                className="text-sm text-strawberry font-[700]"
+                id="emailError"
+              ></div>
             </div>
             <input
               placeholder="e.g. stephenking@lorem.com"
@@ -34,10 +98,14 @@ function PersonalInfo() {
             />
           </div>
           <div className="flex flex-col gap-1">
-            <div>
+            <div className="flex justify-between">
               <label htmlFor="number" className="text-sm">
                 Phone Number
               </label>
+              <div
+                className="text-sm text-strawberry font-[700]"
+                id="numberError"
+              ></div>
             </div>
             <input
               placeholder="e.g. +1 234 567 890"
@@ -48,7 +116,10 @@ function PersonalInfo() {
         </div>
       </div>
       <div className="flex justify-end mb-4">
-        <button className="bg-primary text-white px-7 py-3 rounded-md text-sm font-[500] transition-all duration-200 hover:bg-blue-900">
+        <button
+          className="bg-primary text-white px-7 py-3 rounded-md text-sm font-[500] transition-all duration-200 hover:bg-blue-900"
+          onClick={validate}
+        >
           Next Step
         </button>
       </div>
